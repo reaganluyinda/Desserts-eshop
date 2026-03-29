0895f3d585d676690bd9d03d791b194f01b1137a
reagan luyinda <luyindareagan@gmail.com>
2026-03-29 14:15:29 +0300

    Chore; install morgan to log http requests on the server

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const morgan = require('morgan')
 const productsRouter = require('./controllers/products')
 const app = express()
 
 app.use(express.json())
+app.use(morgan('tiny'))
 app.use('/api/products', productsRouter)
 
 module.exports = app
