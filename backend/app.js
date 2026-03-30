bcfcaa0fad796e4bd278b501bb8f2be5820bca11
reagan luyinda <luyindareagan@gmail.com>
2026-03-30 19:00:44 +0300

    Feat; created middleware file in utils handling logger info and error handling

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
+const middleware = require('./utils/middleware')
 const morgan = require('morgan')
 const productsRouter = require('./controllers/products')
 const app = express()
@@ -16,7 +17,13 @@ mongoose.connect(config.MONGODB_URI, {family: 4})
 })
 
 app.use(express.json())
+app.use(middleware.requestLogger)
 app.use(morgan('dev'))
+
+
 app.use('/api/products', productsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
