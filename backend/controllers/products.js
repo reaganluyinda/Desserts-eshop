7421f6ba87dec4dc467b006cc629bc4ff24e210c
reagan luyinda <luyindareagan@gmail.com>
2026-04-03 14:07:39 +0300

    ref; remove unused products array from products controller

--- backend/controllers/products.js
+++ backend/controllers/products.js
@@ -3,16 +3,6 @@ const productsRouter = require('express').Router()
 const Product = require('../models/product')
 const { error } = require('../utils/logger')
 
-let products = [
-    {
-        id: 1, name: "Product 1", description: "This is product 1", price: 10.99
-       
-    },
-    {
-         id: 2, name: "Product 2", description: "This is product 2", price: 19.99
-    }
-]
-
 
 //fetch all products
 productsRouter.get('/', async(request, response) => {
